1ba1a581981fa83a2161e31c1e7eb15889afa586
Don Bower <don.bower@outlook.com>
2022-10-09 13:47:24 -0700

    add subscription id

--- pushDocker.sh
+++ pushDocker.sh
@@ -2,10 +2,11 @@ cat ~/.ssh/dockerpw | docker login --username donbower --password-stdin
 #
 # Retreive the HCP Team Token from vault
 #
-hcpTeamToken=`vault kv get -format=json concourse/common/tokens/hcpTeam_owners | jq .data.data.value`
-azClientID=`vault kv get -format=json concourse/common/azure/role_Contributor | jq .data.data.appId`
-azClientSecret=`vault kv get -format=json concourse/common/azure/role_Contributor | jq .data.data.password`
-azTenantID=`vault kv get -format=json concourse/common/azure/role_Contributor | jq .data.data.tenant`
+hcpTeamToken=`vault kv get -format=json concourse/common/tokens/hcpTeam_owners | jq --raw-output .data.data.value`
+azSubscriptionID=`vault kv get -format=json concourse/common/azure/role_Contributor | jq --raw-output .data.data.subscriptionId`
+azClientID=`vault kv get -format=json concourse/common/azure/role_Contributor | jq --raw-output .data.data.appId`
+azClientSecret=`vault kv get -format=json concourse/common/azure/role_Contributor | jq --raw-output .data.data.password`
+azTenantID=`vault kv get -format=json concourse/common/azure/role_Contributor | jq --raw-output .data.data.tenant`
 #
 # build the terraformrc file
 #
@@ -24,6 +25,7 @@ export tfVersion=`echo ${thisTag} | cut -d "-" -f 1`
 docker build \
   --build-arg DOCKER_TF_VERSION=${tfVersion} \
   --build-arg DOCKER_TAG=${thisTag} \
+  --build-arg ARM_SUBSCRIPTION_ID=${azSubscriptionID} \
   --build-arg ARM_CLIENT_ID=${azClientID} \
   --build-arg ARM_CLIENT_SECRET=${azClientSecret} \
   --build-arg ARM_TENANT_ID=${azTenantID} \
